f9e498a81ee483b9ae8a4fa1a515cee3088e5b6a
Ryan <rlevels@kent.edu>
2024-09-05 01:30:25 +0000

    more changes

--- topic-03-simple-flask/web-app/app.py
+++ topic-03-simple-flask/web-app/app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, render_template
 
 app = Flask(__name__)
+connection = sqlite3.connect("pets.db", check_same_thread=False)
 
 @app.route("/")
 @app.route("/hello")
 def hello_world():
     return "<p>Hello there, Big World!</p>"
 
+@app.route("/list")
+def get_list():
+    cursor = connection.cursor()
+    cursor.execute("select * from pets")
+    rows = cursor.fetchall()
+
+    output = ""
+    rows =[list(row) for row in rows]
+    print(rows)
+    return Flask.render_template("list.html", prof={"name":"Ryan", "class":"ADSD"}, rows=rows)
+    
 @app.route("/goodbye")
 def goodbye():
     return "<p>Goodbye, then! Have a truly nice day!</p>"
